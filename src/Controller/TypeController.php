70d8a056e45c43b59dfe7531944279a6093aabd1
HeleneHinde <helene.mazgaldi@gmail.com>
2024-11-13 14:01:35 +0100

    création d'un typecontroller et d'une vue pour visualiser la liste de type

---
+++ src/Controller/TypeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\TypeRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class TypeController extends AbstractController
+{
+    #[Route('/type', name: 'app_type')]
+    public function list(TypeRepository $typeRepository): Response
+    {
+
+        $types = $typeRepository->findBy([]);
+
+        return $this->render('type/list.html.twig', [
+            'types' => $types,
+        ]);
+    }
+}
