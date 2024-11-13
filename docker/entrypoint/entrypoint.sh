4844d0c167bb7a4b54527c784b573455384c9f2b
HeleneHinde <helene.mazgaldi@gmail.com>
2024-11-13 14:51:02 +0100

    maj entrypoint.sh avec install yarn

--- docker/entrypoint/entrypoint.sh
+++ docker/entrypoint/entrypoint.sh
@@ -9,6 +9,9 @@ done
 # Exécuter les commandes Symfony
 php bin/console doctrine:database:create --if-not-exists
 php bin/console doctrine:migration:migrate --no-interaction
+git config --global --add safe.directory /var/www/html
+yarn install
+yarn build
 
 # Continuer avec la commande par défaut
 exec "$@"
