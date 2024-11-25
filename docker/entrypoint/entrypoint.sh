9289e45dcc327c0774d254e2b5b3899b3a351dc7
HeleneHinde <helene.mazgaldi@gmail.com>
2024-11-25 11:52:08 +0100

    maj php 8.4

--- docker/entrypoint/entrypoint.sh
+++ docker/entrypoint/entrypoint.sh
@@ -10,6 +10,7 @@ done
 php bin/console doctrine:database:create --if-not-exists
 php bin/console doctrine:migration:migrate --no-interaction
 git config --global --add safe.directory /var/www/html
+git config --global --add safe.directory '*'
 yarn install
 yarn build
 
